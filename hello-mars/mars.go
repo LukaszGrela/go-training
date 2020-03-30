bdb7888da0b01a156bc653c708f65ab727d945b0
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-03-30 23:34:59 +0100

    add a function to calculate mars weight

--- hello-mars/mars.go
+++ hello-mars/mars.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+func toMarsWeight(kg float32) float32 {
+	return kg * 0.3783
+}
+
 func main() {
+	myWeightKg := float32(63.0)
+
 	fmt.Print("My weigt on the surface of Mars is ")
-	fmt.Print(63.0 * 0.3783)
+	fmt.Print(toMarsWeight(myWeightKg))
 	fmt.Println("kg.")
 }
